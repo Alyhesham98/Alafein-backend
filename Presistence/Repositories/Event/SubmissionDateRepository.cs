710e318fd12e1e6c40f0958cd571971664b0ea4b
aemad <aemad@takamol.eg>
2024-11-03 17:43:58 +0200

    filter GetEvents , GetEventsMobile

--- Presistence/Repositories/Event/SubmissionDateRepository.cs
+++ Presistence/Repositories/Event/SubmissionDateRepository.cs
@@ -27,7 +27,7 @@ namespace Presistence.Repositories.Event
 
             if (filter is not null)
             {
-                events = events.Where(filter).OrderBy(o => o.Id);
+                events = events.Where(filter).OrderBy(o => o.Date.Day);
             }
 
             if (parameters.Name is not null)
@@ -37,7 +37,7 @@ namespace Presistence.Repositories.Event
 
                 events = events.Where(f => f.Submission
                                             .EventNameEN
-                                            .Contains(search)).OrderBy(o => o.Id);
+                                            .Contains(search)).OrderBy(o => o.Date.Day);
             }
 
             if (parameters.Venue is not null)
@@ -53,7 +53,7 @@ namespace Presistence.Repositories.Event
                                             .Venue
                                             .User
                                             .LastName
-                                            .Contains(search)).OrderBy(o => o.Id);
+                                            .Contains(search)).OrderBy(o => o.Date.Day);
             }
 
             if (parameters.Organizer is not null)
@@ -67,37 +67,37 @@ namespace Presistence.Repositories.Event
                                            f.Submission
                                             .User
                                             .LastName
-                                            .Contains(search)).OrderBy(o => o.Id);
+                                            .Contains(search)).OrderBy(o => o.Date.Day);
             }
 
             if (parameters.CategoryId is not null)
             {
                 events = events.Where(f => f.Submission
-                                            .CategoryId == parameters.CategoryId).OrderBy(o => o.Id);
+                                            .CategoryId == parameters.CategoryId).OrderBy(o => o.Date.Day);
             }
 
             if (parameters.IsApproved is not null)
             {
                 events = events.Where(f => f.Submission
-                                            .IsApproved == parameters.IsApproved).OrderBy(o => o.Id);
+                                            .IsApproved == parameters.IsApproved).OrderBy(o => o.Date.Day);
             }
 
             if (parameters.IsSpotlight is not null)
             {
                 events = events.Where(f => f.Submission
-                                            .IsSpotlight == parameters.IsSpotlight).OrderBy(o => o.Id);
+                                            .IsSpotlight == parameters.IsSpotlight).OrderBy(o => o.Date.Day);
             }
 
             if (parameters.IsPending is not null)
             {
                 events = events.Where(f => f.Submission
-                                            .Status == SubmissionStatus.PENDING).OrderBy(o => o.Id);
+                                            .Status == SubmissionStatus.PENDING).OrderBy(o => o.Date.Day);
             }
 
             var count = await events.CountAsync();
 
             var data = await events.Skip((parameters.PageNumber - 1) * parameters.PageSize)
-                                   .Take(parameters.PageSize).OrderBy(o => o.Id)
+                                   .Take(parameters.PageSize).OrderBy(o => o.Date.Day)
                                    .Select(s => new ListEventDto
                                    {
                                        Id = s.Id,
@@ -144,7 +144,7 @@ namespace Presistence.Repositories.Event
 
             if (filter is not null)
             {
-                events = events.Where(filter).OrderBy(o => o.Id);
+                events = events.Where(filter).OrderBy(o => o.Date.Day);
             }
 
             if (parameters.Name is not null)
@@ -154,7 +154,7 @@ namespace Presistence.Repositories.Event
 
                 events = events.Where(f => f.Submission
                                             .EventNameEN
-                                            .Contains(search)).OrderBy(o => o.Id);
+                                            .Contains(search)).OrderBy(o => o.Date.Day);
             }
 
             if (parameters.IsFavourite is not null &&
@@ -163,38 +163,38 @@ namespace Presistence.Repositories.Event
                 events = events.Where(f => f.Submission
                                             .FavouriteSubmissions
                                             .Any(c => c.SubmissionId == f.SubmissionId &&
-                                                      c.UserId == userId)).OrderBy(o => o.Id);
+                                                      c.UserId == userId)).OrderBy(o => o.Date.Day);
             }
 
             if (parameters.From is not null)
             {
-                events = events.Where(f => f.Date >= parameters.From).OrderBy(o => o.Id);
+                events = events.Where(f => f.Date >= parameters.From).OrderBy(o => o.Date.Day);
             }
 
             if (parameters.To is not null)
             {
-                events = events.Where(f => f.Date <= parameters.To).OrderBy(o => o.Id);
+                events = events.Where(f => f.Date <= parameters.To).OrderBy(o => o.Date.Day);
             }
 
             if (parameters.CategoryId is not null)
             {
-                events = events.Where(f => f.Submission.CategoryId == parameters.CategoryId).OrderBy(o => o.Id);
+                events = events.Where(f => f.Submission.CategoryId == parameters.CategoryId).OrderBy(o => o.Date.Day);
             }
 
             if (parameters.MinFee is not null)
             {
-                events = events.Where(f => f.Submission.PaymentFee >= parameters.MinFee).OrderBy(o => o.Id);
+                events = events.Where(f => f.Submission.PaymentFee >= parameters.MinFee).OrderBy(o => o.Date.Day);
             }
 
             if (parameters.MaxFee is not null)
             {
-                events = events.Where(f => f.Submission.PaymentFee <= parameters.MaxFee).OrderBy(o => o.Id);
+                events = events.Where(f => f.Submission.PaymentFee <= parameters.MaxFee).OrderBy(o => o.Date.Day);
             }
 
             var count = await events.CountAsync();
 
             var data = await events.Skip((parameters.PageNumber - 1) * parameters.PageSize)
-                                   .Take(parameters.PageSize).OrderBy(o => o.Id)
+                                   .Take(parameters.PageSize).OrderBy(o => o.Date.Day)
                                    .Select(s => new ListEventMobileDto
                                    {
                                        Id = s.Id,
