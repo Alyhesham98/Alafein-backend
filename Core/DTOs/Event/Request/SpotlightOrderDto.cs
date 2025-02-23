19b8a6f12caea4c2e59a3fd19c508483b194b084
aemad <aemad@takamol.eg>
2025-02-23 22:10:13 +0200

    Delete Condition

--- Core/DTOs/Event/Request/SpotlightOrderDto.cs
+++ Core/DTOs/Event/Request/SpotlightOrderDto.cs
@@ -5,7 +5,7 @@ namespace Core.DTOs.Event.Request
     public class SpotlightOrderDto
     {
         public long Id { get; set; }
-        [Range(1, int.MaxValue, ErrorMessage = "Spotlight order must be greater than 0")]
+        //[Range(1, int.MaxValue, ErrorMessage = "Spotlight order must be greater than 0")]
         public int SpotlightOrder { get; set; }
     }
 }
